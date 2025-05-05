8eca9b5d85bb6f98470daadc4d3ca85190d324cb
SEU NOME AQUI <thiago.goncalvesgames@gmail.com>
2025-05-05 19:20:31 -0300

    feat: implemetado vercel.json

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { deleteMissionController, getMissionController, patchMissionStatusController, postMissionController } from './controllers/missionController';
+import { deleteMissionController, getMissionController, patchMissionStatusController, postMissionController } from './src/controllers/missionController';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: "http://26.46.142.72:3000"}));
 
 app.post("/missions", postMissionController);
 app.get("/missions", getMissionController);
